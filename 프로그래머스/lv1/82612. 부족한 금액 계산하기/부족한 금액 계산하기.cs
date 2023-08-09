cc6db271af0855e2316069be7a873dd6038e2980
mjho0225 <52567731+mjho0225@users.noreply.github.com>
2023-08-09 19:06:57 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.17 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Solution
+{
+    public long solution(int price, int money, int count)
+    {
+        long totalCost = 0;
+
+        for (int i = 1; i <= count; i++) {
+            totalCost += (long)price * i;
+        }
+
+        long shortage = totalCost - money;
+        return (shortage > 0) ? shortage : 0;
+    }
+}
