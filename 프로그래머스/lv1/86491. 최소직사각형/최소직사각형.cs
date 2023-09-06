693d2f6d783abcf1865d8c9a0d421d3241cdbe62
mjho0225 <52567731+mjho0225@users.noreply.github.com>
2023-09-06 19:20:38 +0900

    [level 1] Title: 최소직사각형, Time: 0.54 ms, Memory: 32.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Solution {
+    public int solution(int[,] sizes) {
+                int maxHorizontal = 0;
+        int maxVertical = 0;
+
+        for (int i = 0; i < sizes.GetLength(0); i++) {
+            int width = sizes[i, 0];
+            int height = sizes[i, 1];
+
+            maxHorizontal = Math.Max(maxHorizontal, Math.Max(width, height));
+            maxVertical = Math.Max(maxVertical, Math.Min(width, height));
+        }
+
+        int minWalletSize = maxHorizontal * maxVertical;
+        return minWalletSize;
+    }
+}
